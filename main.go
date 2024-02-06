80247e45d888a005e870697ff0f019a9143c8ae2
Alain Liu <luumanhquan.91@gmail.com>
2024-02-07 00:31:09 +0700

    init code

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
+	"os"
+)
+
+func main() {
+	a, _ := os.Open("./imgs/image1.png")
+	img1, err := png.Decode(a)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	b, _ := os.Open("./imgs/image2.png")
+	img2, err := png.Decode(b)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	dst := image.NewRGBA(image.Rect(0, 0, img1.Bounds().Max.X, img1.Bounds().Max.Y))
+
+	draw.Draw(dst, dst.Bounds(), img1, image.Point{0, 0}, draw.Src)
+	draw.Draw(dst, dst.Bounds(), img2, image.Point{0, 0}, draw.Src)
+
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	buf := new(bytes.Buffer)
+	png.Encode(buf, img)
+
+	fmt.Println("Ghép ảnh thành công!", buf.Bytes())
+}
